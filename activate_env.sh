56eb98d6e7b17f2a14f084fe83db4ce1f70a8e7d
alexno-sherlock <alexno@stanford.edu>
2021-10-11 13:50:06 -0700

    add sh script to activate environment

---
+++ activate_env.sh
@@ -0,0 +1,43 @@
+# CURRENT_ENV="narcolepsy-detector"
+# CUDNN_VER="7.0.4"
+
+# if [ $CONDA_DEFAULT_ENV != $CURRENT_ENV ]; then
+#     echo "Unloading current environment"
+#     conda deactivate
+#     echo "Activating environment: $CURRENT_ENV"
+#     conda activate $CURRENT_ENV
+# else
+#     echo "Environment already active"
+# fi
+
+# echo "Loading cuDNN version $CUDNN_VER"
+# ml load cudnn/7.0.4
+
+# echo "Environment ready!"
+
+CURRENT_ENV="nml"
+# TF_VER="2.4.1_py36"
+CUDA_VER="11.2.0"
+CUDNN_VER="8.1.1.33"
+
+# echo "Loading TF version $TF_VER"
+# ml load py-tensorflow/$TF_VER
+
+source $PI_HOME/miniconda3/bin/activate
+if [ $CONDA_DEFAULT_ENV != "base" ]; then
+    echo "Unloading current environment"
+    conda deactivate
+    echo "Activating environment: $CURRENT_ENV"
+    conda activate $CURRENT_ENV
+else
+    echo "Activating environment: $CURRENT_ENV"
+    conda activate $CURRENT_ENV
+fi
+
+echo "Loading CUDA version $CUDA_VER"
+ml load cuda/$CUDA_VER
+
+echo "Loading CUDNN version $CUDNN_VER"
+ml load cudnn/$CUDNN_VER
+
+echo "Environment ready!"
