cbb9ee41f318c36acb8c4d0346eee28072f3ed08
aneol <aneol@dtu.dk>
2024-06-14 10:06:40 +0200

    try black formatting

--- narcolepsy_detector/inference/__main__.py
+++ narcolepsy_detector/inference/__main__.py
@@ -18,7 +18,7 @@ logger = get_logger()
 def predict_single_resolution(file, model_dir, resolution):
 
     # Check the model directories for settings.json
-    updated_model_dir = f'{resolution:07.2f}'.replace('.', '-') if resolution < 1 else f'{resolution:04d}'
+    updated_model_dir = f"{resolution:07.2f}".replace(".", "-") if resolution < 1 else f"{resolution:04d}"
     settings_file = list(model_dir.rglob(f"r{updated_model_dir}*/settings.json"))[0]
     updated_model_dir = settings_file.parent
     with open(settings_file, "r") as f:
@@ -31,38 +31,38 @@ def predict_single_resolution(file, model_dir, resolution):
     # Loop over repeats and folds
     mean_pred = np.zeros((settings["n_repeats"], settings["n_kfold"]))
     var_pred = np.zeros((settings["n_repeats"], settings["n_kfold"]))
-    for i in range(settings['n_repeats']):
-        for j in range(settings['n_kfold']):
+    for i in range(settings["n_repeats"]):
+        for j in range(settings["n_kfold"]):
 
             # Get feature scalers
             scalers_path = updated_model_dir / "feature_scales"
-            if scalers_path.with_suffix('.pkl').exists():
-                scalers_path = scalers_path.with_suffix('.pkl')
+            if scalers_path.with_suffix(".pkl").exists():
+                scalers_path = scalers_path.with_suffix(".pkl")
             elif scalers_path.is_dir():
                 scalers_path = scalers_path / f"feature_scales_n{i + 1:02}_k{j + 1:02}.pkl"
             else:
                 logger.error("No feature scalers found, program stopping!")
                 return -1
-            with open(scalers_path, 'rb') as fp:
+            with open(scalers_path, "rb") as fp:
                 scalers = pickle.load(fp)
 
             # Get PCA objects
             if settings["pca"]:
                 pca_path = updated_model_dir / "pca_objects"
-                if pca_path.with_suffix('.pkl').exists():
-                    pca_path = pca_path.with_suffix('.pkl')
+                if pca_path.with_suffix(".pkl").exists():
+                    pca_path = pca_path.with_suffix(".pkl")
                 elif pca_path.is_dir():
                     pca_path = pca_path / f"pca_objects_n{i + 1:02}_k{j + 1:02}.pkl"
                 else:
                     logger.error("No PCA objects found, program stopping!")
                     return -1
-                with open(pca_path, 'rb') as fp:
+                with open(pca_path, "rb") as fp:
                     pcas = pickle.load(fp)
 
             # Get model scale weights
             model_weights_path = updated_model_dir / "model_scales"
-            if model_weights_path.with_suffix('.csv').exists():
-                model_weights_path = model_weights_path.with_suffix('.csv')
+            if model_weights_path.with_suffix(".csv").exists():
+                model_weights_path = model_weights_path.with_suffix(".csv")
             elif model_weights_path.is_dir():
                 model_weights_path = model_weights_path / f"model_scales_n{i + 1:02}_k{j + 1:02}.csv"
             else:
@@ -72,14 +72,14 @@ def predict_single_resolution(file, model_dir, resolution):
 
             # Get model. Baseline models are saved as .lrm files, while GP models are saved as .gpm files
             model_path = updated_model_dir / "models" / f"model_n{i + 1:02}_k{j + 1:02}.pkl"
-            if settings['baseline_model']:
+            if settings["baseline_model"]:
                 model_path = model_path.with_suffix(".lrm")
-                with open(model_path, 'rb') as pkl:
+                with open(model_path, "rb") as pkl:
                     loaded_model = pickle.load(pkl)
             else:
                 model_path = model_path.with_suffix(".gpm")
                 with warnings.catch_warnings():
-                    warnings.simplefilter('ignore')
+                    warnings.simplefilter("ignore")
                     loaded_model = tf.saved_model.load(model_path)
 
             # Scale data
@@ -94,25 +94,33 @@ def predict_single_resolution(file, model_dir, resolution):
             # If feature selection procedure was performed, select correct features
             if settings["feature_selection"]:
                 fs_path = updated_model_dir / "feature-selection" / f"selected-features_n{i + 1:02}_k{j + 1:02}.pkl"
-                with open(fs_path, 'rb') as fp:
-                    selected_features = np.array(pickle.load(fp)['features']).astype(int)
+                with open(fs_path, "rb") as fp:
+                    selected_features = np.array(pickle.load(fp)["features"]).astype(int)
                 selected_features = (
                     selected_features[: len(selected_features) // 2] + selected_features[len(selected_features) // 2 :]
                 ) > 0
                 X_scaled = X_scaled[:, selected_features]
 
             # Get predictions
-            if settings['baseline_model']:
+            if settings["baseline_model"]:
                 mean_pred[i, j] = loaded_model.predict_proba(X_scaled)[:, -1]
                 var_pred[i, j] = np.zeros(mean_pred[i, j].shape)
             else:
                 with warnings.catch_warnings():
-                    warnings.simplefilter('ignore')
+                    warnings.simplefilter("ignore")
                     mean_pred[i, j], var_pred[i, j] = loaded_model.predict_y_compiled(X_scaled)
 
     # Average over folds and apply scaling to each repeat
-    p_pred = (mean_pred.mean(axis=1).T @ model_weights.values / model_weights.values.sum()).squeeze() if 'model_weights' in locals() else mean_pred.mean(axis=1).squeeze()
-    v_pred = (var_pred.mean(axis=1).T @ model_weights.values / model_weights.values.sum()).squeeze() if 'model_weights' in locals() else var_pred.mean(axis=1).squeeze()
+    p_pred = (
+        (mean_pred.mean(axis=1).T @ model_weights.values / model_weights.values.sum()).squeeze()
+        if "model_weights" in locals()
+        else mean_pred.mean(axis=1).squeeze()
+    )
+    v_pred = (
+        (var_pred.mean(axis=1).T @ model_weights.values / model_weights.values.sum()).squeeze()
+        if "model_weights" in locals()
+        else var_pred.mean(axis=1).squeeze()
+    )
 
     return p_pred, v_pred, None
 
@@ -131,6 +139,7 @@ def compute_ensemble(probs, vars, aucs, ensembling_method):
         raise NotImplementedError("AUC-inv ensembling not yet implemented")
 
     return p_ens, v_ens
+
 
 def run_inference(data_dir, model_dir, savedir_output, resolutions, ensembling_method):
 
@@ -151,7 +160,7 @@ def run_inference(data_dir, model_dir, savedir_output, resolutions, ensembling_m
         for resolution in resolutions:
             m, v, auc = predict_single_resolution(file, model_dir, resolution)
 
-            file_ids.append(file.stem.split('preds_')[1])
+            file_ids.append(file.stem.split("preds_")[1])
             probs.append(m)
             vars.append(v)
             res.append(resolution)
@@ -159,7 +168,7 @@ def run_inference(data_dir, model_dir, savedir_output, resolutions, ensembling_m
 
         # Save ensemble predictions
         p_ens, v_ens = compute_ensemble(probs, vars, aucs, ensembling_method)
-        file_ids.append(file.stem.split('preds_')[1])
+        file_ids.append(file.stem.split("preds_")[1])
         probs.append(p_ens)
         vars.append(v_ens)
         res.append("ensemble")
@@ -167,15 +176,15 @@ def run_inference(data_dir, model_dir, savedir_output, resolutions, ensembling_m
 
     print("Saving predictions to disk...")
     df = pd.DataFrame({"file_id": file_ids, "resolution": res, "prob": probs, "var": vars, "auc": aucs})
-    df = (pd.concat([
-        df.query('resolution != "ensemble"')
-          .sort_values(['resolution', 'file_id']),
-        df.query('resolution == "ensemble"')
-          .sort_values('file_id')
-        ]).reset_index(drop=True)
-    )
+    df = pd.concat(
+        [
+            df.query('resolution != "ensemble"').sort_values(["resolution", "file_id"]),
+            df.query('resolution == "ensemble"').sort_values("file_id"),
+        ]
+    ).reset_index(drop=True)
     savedir_output.mkdir(exist_ok=True, parents=True)
     df.to_csv(savedir_output / "predictions.csv", index=False)
+
 
 def main_cli():
     """
@@ -188,5 +197,4 @@ def main_cli():
 
 
 if __name__ == "__main__":
-
     main_cli()
